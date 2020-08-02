b919b82bcefeee0c64d8027084e1aab8d53ee9df
Michał Kurcewicz <mkurc1@gmail.com>
2020-08-02 21:41:17 +0200

    Fix problem with special chars in mysql password

--- src/service/db_dumper.py
+++ src/service/db_dumper.py
@@ -8,8 +8,8 @@ class DbDumper:
         file_name = f'{self.__prepare_file_name(db_name)}.sql.gz'
         file_path = f'{dump_dir}/{file_name}'
 
-        cmd = f'docker exec {container} /usr/bin/mysqldump -u root --password={root_pass} {db_name} ' \
-              f'| gzip -c > {file_path}'
+        cmd = f"docker exec {container} /usr/bin/mysqldump -uroot -p'{root_pass}' {db_name} " \
+              f"| gzip -c > {file_path}"
 
         process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stderr = process.communicate()[1]
@@ -24,4 +24,3 @@ class DbDumper:
     def __prepare_file_name(name):
         now = datetime.now()
         return f'{name.lower()}_{now.strftime("%d_%m_%Y-%H_%M_%S")}'
-
